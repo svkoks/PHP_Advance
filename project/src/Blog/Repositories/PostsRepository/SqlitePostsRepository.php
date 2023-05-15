ba68f9cf415c7d8b8e7b412d7f71c57701c4c185
svkoks <62989718+svkoks@users.noreply.github.com>
2023-05-15 09:13:17 +0000

    update homework for http, to add function "delete" in SqlitePostsRepository

--- project/src/Blog/Repositories/PostsRepository/SqlitePostsRepository.php
+++ project/src/Blog/Repositories/PostsRepository/SqlitePostsRepository.php
@@ -67,4 +67,15 @@ class SqlitePostsRepository implements PostsRepositoryInterface
             $result['text']
         );
     }
+
+    public function delete(UUID $uuid): void
+    {
+        $statement = $this->connection->prepare(
+            'DELETE FROM posts WHERE posts.uuid=:uuid;'
+        );
+
+        $statement->execute([
+            ':uuid' => $uuid,
+        ]);
+    }
 }
